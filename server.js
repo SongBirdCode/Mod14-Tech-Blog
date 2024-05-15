14181b4c196076a4bf4c01ca9e81226b33827004
SongBirdCode <Songbirdcode@gmail.com>
2024-05-14 20:57:50 -0400

    Fixed server issue and finessed handlebars

--- server.js
+++ server.js
@@ -4,11 +4,12 @@ const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const helpers = require('./utils/helpers');
+const sequelize = require("./config/connection");
+const SequelizeStore = require('connect-session-sequelize')(session.Store)
 // Set up const and dependencies //
 
+
 // Set up sequelize config and session key storing // 
-const sequelize = require('./config/connection');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -19,12 +20,31 @@ const hbs = exphbs.create({ helpers });
 const sess = {
   secret: 'Super secret secret',
   cookie: {},
+  maxAge: 300000,
   resave: false,
+  sameSite:"strict",
   saveUninitialized: true,
   store: new SequelizeStore({
     db: sequelize
   })
 };
+
+// Created homepage route and login route
+app.get("/", (req,res) => {
+  const logged_in = true
+ res.render("homepage", {logged_in})
+});
+
+app.get("/signup", (req,res) => {
+ const logged_in = true
+ res.render("signup", {logged_in})
+});
+
+app.get("/login", (req,res) => {
+ const logged_in = true
+ res.render("login", {logged_in})
+});
+
 
 app.use(session(sess));
 
